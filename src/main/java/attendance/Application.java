acf25eaa65387538072dcb748936bafda0175d28
Jang_Laptop <sdsd090811@gmail.com>
2024-12-14 17:59:10 +0900

    feat: 완성된 부분까지 커밋

--- src/main/java/attendance/Application.java
+++ src/main/java/attendance/Application.java
@@ -1,7 +1,61 @@
 package attendance;
 
+import attendance.attendance.HistoryFormatter;
+import attendance.io.DateReader;
+import attendance.io.NicknameReader;
+import attendance.io.attendance.AttendancesFileReader;
+import attendance.io.confirmation.ConfirmationReader;
+import attendance.io.function.FunctionSelection;
+import attendance.io.function.FunctionSelectionReader;
+import attendance.io.InputReader;
+import attendance.io.modification.ModificationReader;
+import attendance.myfunctions.ConfirmationFunction;
+import attendance.myfunctions.CrewHistoryFunction;
+import attendance.myfunctions.ModificationFunction;
+
 public class Application {
+    private static CrewAttendanceRepository repository = crewAttendanceRepository();
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        FunctionSelectionReader functionSelectionReader = functionSelectionReader();
+        ConfirmationReader confirmationReader = new ConfirmationReader(inputReader(), repository);
+
+        FunctionSelection function;
+        ConfirmationFunction confirmationFunction = new ConfirmationFunction(confirmationReader, repository);
+        ModificationReader reader = new ModificationReader(inputReader(), nicknameReader(), dateReader());
+        ModificationFunction modificationFunction = new ModificationFunction(reader, repository);
+        CrewHistoryFunction crewHistoryFunction = new CrewHistoryFunction(nicknameReader(), repository, new HistoryFormatter());
+        do {
+            function = functionSelectionReader.read();
+            if(function.equals(FunctionSelection.CONFIRMATION))
+                confirmationFunction.apply();
+            if(function.equals(FunctionSelection.MODIFICATION))
+                modificationFunction.apply();
+            if(function.equals(FunctionSelection.CREW_HISTORY))
+                crewHistoryFunction.apply();
+        } while (!function.equals(FunctionSelection.QUIT));
+    }
+
+    private static NicknameReader nicknameReader() {
+        return new NicknameReader(inputReader(), repository);
+    }
+
+    private static DateReader dateReader() {
+        return new DateReader(inputReader());
+    }
+
+    private static CrewAttendanceRepository crewAttendanceRepository() {
+        return new CrewAttendanceRepository(fileReader());
+    }
+
+    private static AttendancesFileReader fileReader() {
+        return new AttendancesFileReader();
+    }
+
+    private static FunctionSelectionReader functionSelectionReader() {
+        return new FunctionSelectionReader(inputReader());
+    }
+
+    private static InputReader inputReader() {
+        return new InputReader();
     }
 }
