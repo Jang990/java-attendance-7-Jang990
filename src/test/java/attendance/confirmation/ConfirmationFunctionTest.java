84ebf2cab4fea79cd1a3ec2a4c05597aaef66a84
Jang_Laptop <sdsd090811@gmail.com>
2024-12-14 16:36:23 +0900

    fix: 중복 테스트 코드 제거

--- src/test/java/attendance/confirmation/ConfirmationFunctionTest.java
+++ src/test/java/attendance/confirmation/ConfirmationFunctionTest.java
@@ -33,16 +33,4 @@ class ConfirmationFunctionTest {
         assertTrue(repository.get("쿠키").isAlreadyAttended(time.toLocalDate()));
     }
 
-    @Test
-    void 출석_확인_기능() {
-        LocalDateTime time = LocalDateTime.of(2024, 12, 6, 10, 0, 0);
-        ConfirmationReaderStub confirmationReaderStub = new ConfirmationReaderStub(
-                new ConfirmationDto("쿠키", time));
-        ConfirmationFunction function = new ConfirmationFunction(confirmationReaderStub, repository);
-
-        function.apply();
-
-        assertTrue(repository.get("쿠키").isAlreadyAttended(time.toLocalDate()));
-    }
-
 }
